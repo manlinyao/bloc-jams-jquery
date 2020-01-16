17a79f23f9e3b724c2a935cee3ac8d174f7a8927
Manlin Yao <manlinyao18@gmail.com>
2020-01-16 15:55:02 -0600

    assignment 18 completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,20 +1,9 @@
 class Helper {
-<<<<<<< HEAD
-  playPauseAndUpdate(song) {
-    const duration = player.getDuration();
-    $('#time-control .total-time').text ( duration );
-    player.playPause(song);
-    };
-
-  }
-  const helper = new Helper();
-=======
   playPauseAndUpdate (song) {
     const duration = player.getDuration();
     $('#time-control .total-time').text( duration );
     player.playPause(song);
-  };
-}
+    };
+  }
 
-const helper = new Helper();
->>>>>>> checkpoint-18-sliders
+  const helper = new Helper();
